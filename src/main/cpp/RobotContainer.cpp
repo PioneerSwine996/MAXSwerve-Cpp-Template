ddec799ffcaf8dc2a168093066779966b1097e21
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-01-30 16:52:20 -0800

    Trying to fix Arm

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -66,11 +66,11 @@ void RobotContainer::ConfigureButtonBindings() {
   frc2::JoystickButton(&m_driverController,1)
       .WhileTrue(new frc2::RunCommand([this] { m_drive.SetX(); }, {&m_drive}));
   frc2::JoystickButton(&m_driverController, 3)
-      .WhileTrue(new frc2::RunCommand([this] { m_arm.set(-0.3, 0);}, {&m_arm}));
+      .OnTrue(new frc2::RunCommand([this] { m_arm.set(-0.3, 0);}, {&m_arm}));
   frc2::JoystickButton(&m_driverController, 4)
-      .WhileTrue(new frc2::RunCommand([this] { m_arm.set(0.3, 0);}, {&m_arm}));
+      .OnTrue(new frc2::RunCommand([this] { m_arm.set(0.3, 0);}, {&m_arm}));
   frc2::JoystickButton(&m_driverController, 5)
-      .WhileTrue(new frc2::RunCommand([this] {m_arm.setWheel(0.3);}, {&m_arm}));
+      .OnTrue(new frc2::RunCommand([this] {m_arm.setWheel(0.3);}, {&m_arm}));
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
